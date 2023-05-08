82b110dbaaaceca11713e1ab86d20b57b98f72cc
Rogelio Camargo <rogelio.camargo2016@gmail.com>
2023-05-07 21:40:45 -0700

    Configure prettier plugin for talwind css that auto sorts classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="m-2 bg-red-500 p-2 text-blue-500">
       <div>Hello, World</div>
     </main>
-  )
+  );
 }
